772b3bbf29afdd20534644a88065ef9a2c5a6200
arsalandanish0903 <97827499+arsalandanish0903@users.noreply.github.com>
2023-11-21 09:20:45 +0000

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 12453;
+let accountEmail = "admin@gmail.com";
+var accountPassword = "Password";
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+let name = "Arsalan"; // this is string
+let age = 20; // thi is nnumber
+let isLoggedIn = true; // boolean
+let city; // undefined
+let alone = null; // standalone value
+console.log(name);
