5098bcdf4da4feb04baac4a60f53756bb1697cd4
arsalandanish0903 <97827499+arsalandanish0903@users.noreply.github.com>
2023-11-22 06:27:42 +0000

    conversion dataTypes

---
+++ 01_basics/03_conversionNumber.js
@@ -0,0 +1,11 @@
+let score = 33;
+let isLoggedIn = Number(score);
+//console.log(isLoggedIn);
+
+let isBoolean = 1;
+let isoutput = Boolean(isBoolean);
+//console.log(isoutput);
+
+let age = 20;
+let ageOutput = String(age);
+console.log(typeof age);
