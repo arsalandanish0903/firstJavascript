17995068d16831f1aa42b271b8f0c45e84d4284a
arsalandanish0903 <97827499+arsalandanish0903@users.noreply.github.com>
2023-11-23 09:16:53 +0000

    strings

---
+++ 01_basics/04_string.js
@@ -0,0 +1,23 @@
+// const firstName = "Arsalan";
+// const lastName = "Danish";
+// const fullName = `my name is ${firstName} ${lastName}`;
+// console.log(fullName);
+// concatinate 
+
+
+// const stringOne = "JavaScript";
+// console.log(stringOne.slice(0,4));
+// this is slice method
+
+// const stringTwo = "Hyper Text Markeup Language";
+// console.log(stringTwo.indexOf("M"));
+// indexOf method
+
+// const stringThree = "    python          ";
+// console.log(stringThree);
+// console.log(stringThree.trim());
+// this is trim mehthod
+
+// const url = "https://javaScript.com/arsalan%20danish";
+// console.log(url.replace("%20","-"));
+// this is replace method
