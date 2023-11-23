8ecbdfdbf685b5a8b04125760698cd41706b67a8
arsalandanish0903 <97827499+arsalandanish0903@users.noreply.github.com>
2023-11-23 09:55:54 +0000

    math and num

---
+++ 01_basics/05_number_math.js
@@ -0,0 +1,21 @@
+// const otherNumber = 123.899;
+// console.log(otherNumber.toPrecision(3));
+// toPrecision method
+
+// const hundred = 1000000;
+// console.log(hundred.toLocaleString("en-IN"));
+// this is toLocaleString method
+
+
+// --------------------------------Math-----------------------------
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// Math.random() hamesha 0.1 0.2 0.4 hosakti hai
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
