12e106a923f6ac22b0da427070a06d0baef76877
arsalandanish0903 <97827499+arsalandanish0903@users.noreply.github.com>
2023-11-25 07:23:31 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,20 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+console.log(myArr);
+
+// myArr.push("Arsalan");
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift("Danish")
+// console.log(myArr);
+
+// myArr.shift();
+// console.log(myArr);
+
+// slice or splice
+
+console.log(myArr.slice(1,3));
+console.log(myArr.splice(1,3));
+
