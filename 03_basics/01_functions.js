d8efca705fb88e301bbddffab1950be61ea6ee8d
arsalandanish0903 <97827499+arsalandanish0903@users.noreply.github.com>
2023-11-28 08:57:21 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,46 @@
+// function myFunction(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(myFunction(10, 20));
+
+
+// function userLoggin(username){
+//     if(!username){
+        
+//     }
+//     return `${username} just logged in`;
+// }
+// console.log(userLoggin());
+
+
+// function calculateCartPrice(...num1){ // this is spread operator
+//     return num1
+// }
+// console.log(calculateCartPrice(100, 200, 300))
+
+// const details = {
+//     name: "Danish",
+//     price: 999
+// }
+
+// function myObjFunction(anyObject){
+//     return `my name is ${anyObject.name} and this price is ${anyObject.price}`
+// }
+// console.log(myObjFunction(details))
+
+// function newObj(myObj) {
+//     console.log(`my name is ${myObj.name} and this price is ${myObj.price}`);
+// }
+
+// newObj({
+//     name : "Arsalan",
+//     price : 399
+// })
+
+
+const myArray = [10, 20, 30, 40];
+
+function myArrayFunction(anyArray){
+    return anyArray[1]
+}
+console.log(myArrayFunction(myArray));
